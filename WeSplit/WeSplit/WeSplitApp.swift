15b8bc56259e8bcbb65cbcc2725c98bbdd739fe8
Esam Sherif <esam_sherif@hotmail.com>
2022-04-14 06:46:43 +0200

    Initial commit

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Esam Sherif on 4/14/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
