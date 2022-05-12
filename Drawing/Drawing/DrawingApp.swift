d79bdf4be165475df94d28f3d3275420be2e43a1
Esam Sherif <esam_sherif@hotmail.com>
2022-05-12 23:10:33 +0200

    Drawing-Implementation

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Esam Sherif on 5/10/22.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
