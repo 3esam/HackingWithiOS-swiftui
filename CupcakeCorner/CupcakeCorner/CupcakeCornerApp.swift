e220248d3ecda5266ef492b550abd3a9cea1c34e
Esam Sherif <esam_sherif@hotmail.com>
2022-05-14 11:22:13 +0200

    CupcakeCorner-Overview

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Esam Sherif on 5/13/22.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
