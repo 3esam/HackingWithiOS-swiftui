effe9c07ab6b5f4b19b29b9df542b264a583d8e7
Esam Sherif <esam_sherif@hotmail.com>
2022-08-16 22:18:43 +0200

    BucketList-Overview

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Esam Sherif on 8/16/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
